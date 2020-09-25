8e434fd721c5e3f4f1347a08aa6d558f60c3eb44
Oclemy <oclemmi@gmail.com>
2020-09-25 19:32:26 +0300

    Updated the example

--- app/src/main/java/info/camposha/gallerina/MainActivity.kt
+++ app/src/main/java/info/camposha/gallerina/MainActivity.kt
@@ -6,14 +6,12 @@ import info.camposha.galleryna.GalleryActivity
 
 class MainActivity : GalleryActivity() {
 
-    private fun photos() = (169..216)
+    private fun images() = (169..216)
         .map { "https://picsum.photos/1000/700?image=$it" }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Config.setImageURLs(photos().toTypedArray())
+        Config.setImageURLs(images().toTypedArray())
     }
-
-
 }
